4a2ff514b86acfc3f67ffa924fc3eca01e9bc5d5
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-20 17:23:47 +0100

    ADD: some pages components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {SearchComponent} from "./search/search.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'home',
+  component:HomeComponent,
+  pathMatch:'full'
+},{
+  path:'search',
+  component:SearchComponent,
+  pathMatch:'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
