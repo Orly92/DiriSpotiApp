63b282ca29c04b34c4e19184a10d6393d0a5482d
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-24 13:40:15 +0100

    ADD: handler menu toggle in mobil view

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'header',
+  selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  showMenu: boolean;
 
-  constructor() { }
+  @ViewChild("menu")
+  protected menu: ElementRef | undefined;
+
+  constructor(protected renderer:Renderer2) {
+    this.showMenu = false;
+  }
 
   ngOnInit(): void {
   }
 
+  sidebarToggle() {
+    let removeClassName = "showMenu";
+    let addClassName = "hideMenu";
+    if (!this.showMenu) {
+      removeClassName = "hideMenu";
+      addClassName = "showMenu";
+    }
+    this.renderer.removeClass(this.menu?.nativeElement,removeClassName);
+    this.renderer.addClass(this.menu?.nativeElement,addClassName);
+    this.showMenu = !this.showMenu;
+  }
 }
